06734cd902604eeb9370f037c7ed5c6ca5656c57
shangchengshuai <threescs@163.com>
2020-03-04 17:33:11 +0800

    Create 电话号码组合.js

---
+++ 电话号码组合.js
@@ -0,0 +1,28 @@
+export default (str) => {
+    let map = ['', 1, 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
+    let num = str.split('')
+    let code = []
+    // 映射数组
+    num.forEach(item => {
+        if(map[item]) {
+            code.push(map[item])
+        }
+    })
+    let comb = (arr) => {
+        // 临时变量保存前两个组合的结果
+        let tmp = []
+        for (let i = 0, il = arr[0].length; i<il; i++) {
+            for (let j = 0, jl = arr[1].length; j < jl; j++ ) {
+                tmp.push(`${arr[0][i]}${arr[1][j]}`)
+            }
+        }
+        arr.splice(0,2,tmp)
+        if(arr.length > 1) {
+            comb(arr)
+        }else {
+            return tmp;
+        }
+        return arr[0];
+    } 
+    return comb(code)
+}
