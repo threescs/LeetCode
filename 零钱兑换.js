1ea16180bf453504af681ba7320ea1e961adfc47
shangchengshuai <threescs@163.com>
2020-03-08 18:55:22 +0800

    Create 零钱兑换.js

---
+++ 零钱兑换.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function(coins, amount) {
+    let dp = [0]
+    for (let i = 1; i<amount + 1; i++) {
+        dp[i] = Number.MAX_VALUE
+        for(let coin of coins) {
+            if(i>=coin && dp[i-coin] !== -1) {
+                dp[i] = Math.min(dp[i-coin] + 1, dp[i])
+            }
+        }
+        if(dp[i] === Number.MAX_VALUE) {
+            dp[i] = -1
+        }
+    }
+    return dp[amount]
+};
